bb1f3088ea607d9e4356fcee3c2dd6b67193af2a
orifha003 <orifha03mulovhedzi@gmail.com>
2026-07-04 23:31:24 +0200

    update main.py

--- main.py
+++ main.py
@@ -7,3 +7,22 @@ def generate_password(min_Length, numbers=True, special_characters=True):
     special = string.punctuation
 
     
+    characters = letters 
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters+=special
+
+    pwd=""
+    meet_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meet_criteria or len(pwd) < min_Length:
+        new_char = random.choice (characters)
+        pwd += new_char
+
+        if new_char in digits: 
+            has_number = True
+        elif new_char in special:
+            has_special = True
